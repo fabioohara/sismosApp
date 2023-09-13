ba7c4f1400126199e4a46f0ad86b9f459e7cf361
unknown <a.rojas@datgeotech.com>
2023-09-12 19:16:48 -0500

    2023_09_12_B

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,6 +1,6 @@
 import moment from 'moment/moment'
-const today=moment().add('days', -3).format("YYYY-MM-DD");
-const tomorrow=moment().add('days', 1).format("YYYY-MM-DD");
+const today=moment().add('days', -2).format("YYYY-MM-DD");
+const tomorrow=moment().add('days', 2).format("YYYY-MM-DD");
 const BASE_URL="https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=2.45&starttime="+today+"&endtime="+tomorrow
 //const BASE_URL="https://raw.githubusercontent.com/fabioohara/json/main/sismos.json"
 export const getDataFromSismo =async(url=BASE_URL)=>{
